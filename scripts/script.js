4cc5020cb091bab89e638532a0eb21dc797b65ca
ajona <dodoajona@gmail.com>
2026-07-07 13:34:04 +0100

    Code the js side of things.. might be broken expect some fixes in later version

---
+++ scripts/script.js
@@ -0,0 +1,197 @@
+let transactions = [
+
+];
+
+let monthlyIncome = 22876;
+let monthlyExpenses = 786487;
+
+const today = new Date().toISOString().split("T")[0];
+console.log(today);
+document.getElementById("incomeDate").value = today;
+document.getElementById("expenseDate").value = today;
+
+function openIncomeModel() {
+  document.getElementById("incomeModel").style.display = "block";
+  document.body.style.overflow = "hidden";
+  console.log("user");
+}
+function openExpenseModel() {
+  document.getElementById("expenseModel").style.display = "block";
+  document.body.style.overflow = "hidden";
+  console.log("userExpense");
+}
+function closeModel(modelId) {
+  document.getElementById(modelId).style.display = "none";
+  document.body.style.overflow = "auto";
+
+  if (modelId === "incomeModel") {
+    document.getElementById("incomeForm").reset();
+    document.getElementById("incomeDate").value = today;
+  } else {
+    document.getElementById("expenseForm").reset();
+    document.getElementById("expenseDate").value = today;
+  }
+}
+window.onclick = function (event) {
+  const incomeModel = document.getElementById("incomeModel");
+  const expenseModel = document.getElementById("expenseModel");
+
+  if (event.target === incomeModel) {
+    closeModel("incomeModel");
+  }
+  if (event.target === expenseModel) {
+    closeModel("expenseModel");
+  }
+};
+
+function addIncome() {
+  const amount = parseFloat(document.getElementById("incomeAmount").value);
+  const category = document.getElementById("incomeCategory").value;
+  const description = document.getElementById("incomeDescription").value;
+  const date = document.getElementById("incomeDate").value;
+
+  if (!amount || !category || !date) {
+    alert("Please fill in all required fields");
+    return;
+  }
+  const newTransaction = {
+    id: transactions.length + 1,
+    date: date,
+    category: category.charAt(0).toUpperCase + category.slice(1),
+    amount: amount,
+    status: "success",
+    type: "income",
+    description: "desctription",
+  };
+  transactions.unshift(newTransaction);
+
+  monthlyIncome += amount;
+  updateDashboard();
+  updateTransactionsTable();
+
+  closeModel("incomeModel");
+  showNotification("income added successfully", "success");
+}
+
+function addExpense() {
+  const amount = parseFloat(document.getElementById("expenseAmount").value);
+  const category = document.getElementById("expenseCategory").value;
+  const description = document.getElementById("expenseDescription").value;
+  const date = document.getElementById("expenseDate").value;
+
+  if (!amount || !category || !date) {
+    alert("Please fill in all required fields");
+    return;
+  }
+  const newTransaction = {
+    id: transactions.length + 1,
+    date: date,
+    category: category.chartAt(0).toUpperCase + category.slice(1),
+    amount: -amount,
+    status: "success",
+    type: "expense",
+    description: "desctription",
+  };
+  transactions.unShift(newTransaction);
+
+  monthlyExpenses += amount;
+  updateDashboard();
+  updateTransactionsTable();
+
+  closeModel("incomeModel");
+  showNotification("income added successfully", "success");
+}
+
+function updateDashboard() {
+  document.querySelector(".income-amount").textContent =
+    `${monthlyIncome.toLocaleString()}.00`;
+  document.querySelector(".expense-amount").textContent =
+    `${monthlyExpenses.toLocaleString()}.00`;
+
+  let spendingLimit = 67678;
+  const usedAmount = monthlyExpenses;
+  const percentage = (usedAmount / spendingLimit) * 100;
+  document.querySelector(".spending-limit").textContent =
+    `${(spendingLimit - usedAmount).toLocaleString()}.00`;
+  document.querySelector(".progress-fill").style.width =
+    `${Math.min(percentage, 100)}%`;
+}
+
+function updateTransactionsTable() {
+  const tbody = document.querySelector(".transactions-table tbody");
+  tbody.innerHTML = "";
+
+  const recentTransactions = transactions.slice(0, 10);
+  recentTransactions.forEach((transaction) => {
+    const row = document.createElement("tr");
+    const formattedDate = new Date(transaction.date).toLocaleDateString(
+      "en-Us",
+      {
+        month: "short",
+        day: "numeric",
+        year: "numeric",
+      },
+    );
+    const amountDisplay =
+      transaction > 0
+        ? "+${transaction.amount.toLocaleString()}.00"
+        : `-${Math.abs(transaction.amount).toLocaleString()}.00`;
+
+    row.innerHTML = `
+    <tr>
+              <td>${formattedDate}</td>
+              <td>${transaction.category}</td>
+              <td style="color: ${transaction.amount > 0 ? '#10b981' : '#ef4444' }">${amountDisplay}</td>
+              <td><span class="status-success">${transaction.status}</span></td>
+              <td>
+                <button class="action-btn">
+                  <i class="fa-solid fa-ellipsis"></i>
+                </button>
+              </td>
+            </tr>
+    `;
+    tbody.appendChild(row)
+  });
+}
+
+function showNotification(message, type = 'success'){
+  const  notification = document.createElement('div')
+  notification.style.cssText = `
+    position:fixed;
+    top:2rem;
+    right:2rem;
+    color:white;
+    padding:1rem 1.5rem;
+    border-radius: 8px;
+    box-shadow: 0 4px 12px rgba(0, 0, 0, .5);
+    z-index:1001;
+    animation:slideInRight .3s ease;
+    background: ${type === 'success' ? '#10b981' : '#ef4444'} ;
+  `
+  notification.textContent = message;
+  document.body.appendChild(notification);
+
+  setTimeout(()=>{
+    notification.style.animation = 'slideOutRight .3s ease'
+    setTimeout(()=>{
+      document.body.removeChild(notification)
+    }, 3000)
+  }, 3000)
+}
+
+const style = document.createElement('style');
+style.textContent= `
+  @keyframes slideInRight {
+  from{transform:translateX(100%); opacity:0;}
+  to{transform:translateX(0); opacity:1;}
+  }
+  @keyframes slideOutRight {
+    from{transform:translateX(0%); opacity:1;}
+    to{transform:translateX(100%); opacity:0;}
+  }
+
+`
+document.head.appendChild(style);
+document.addEventListener('DOMContentLoaded', function(){
+  updateTransactionsTable()
+})
